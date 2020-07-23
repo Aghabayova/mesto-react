a35943d4c0dadb6027c75daefaf34bf21c234e23
Samira Aghabayova <aghabayova.samira@gmail.com>
2020-07-23 13:53:05 -0400

    fix: ReadMe

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,12 @@ function Card(props) {
     function handleClick() {
         props.onClick(props.card)
     }
+    const imageStyle = {
+        backgroundImage: 'url(' + props.card.link + ')',
+      };
     return (
         <div className="card" key={props.card._id}>
-            <img className="card__image" onClick={handleClick} src={props.card.link} alt={props.card.name} />
+            <div className="card__image" onClick={handleClick} style={imageStyle} />
             <div className="card__description">
                 <h3 className="card__title">{props.card.name}</h3>
                 <div className="card__like">
